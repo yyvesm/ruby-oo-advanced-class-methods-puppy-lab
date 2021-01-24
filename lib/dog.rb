c49f708ad0467352b7e079619aa2d78754ce38ad
yves <yvesmomplais@hotmail.com>
2021-01-24 18:09:15 -0500

    commit tree 723ae94e4ebd2524230710245d2d018ce5163f8b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        puts @@all.map{ |name| name.name}
+    end
+
+    def save
+        self.class.all << self
+      end
+    
+
+end 
